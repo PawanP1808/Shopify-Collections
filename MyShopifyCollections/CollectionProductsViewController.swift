8f421ccbfa7d3a6e3b11571a921d2a2f3a8ce5a6
Pawan Patel <patelpawan1996@hotmail.com>
2019-01-11 09:55:46 -0500

    removed all force unwrapping

--- MyShopifyCollections/CollectionProductsViewController.swift
+++ MyShopifyCollections/CollectionProductsViewController.swift
@@ -28,58 +28,28 @@ class CollectionProductsViewController:UIViewController,UITableViewDelegate,UITa
 
 	var collectionID:Int?
 
-	var collectionsData:[Product]?
-
 	var tableViewData = [cellData]()
 
 
 	override func viewDidLoad() {
-
-		self.productsTableView.rowHeight = 60
-
-//		self.tableViewData = [cellData(opened: false, title: "title1", sectionData: ["cell1","cell2","cell3"]),
-//					  cellData(opened: false, title: "title1", sectionData: ["cell1","cell2","cell3"]),
-//					  cellData(opened: false, title: "title1", sectionData: ["cell1","cell2","cell3"])]
-
-//		print(self.collectionID)
-		dataManager.getProductIdsForCollection(id: self.collectionID!) { success,data in
-			if (success) {
-				self.dataManager.getProductDataForIds(ids: data!){ success, data in
-
-					self.collectionsData = data
+		guard let id = collectionID else {return}
+		dataManager.getProductIdsForCollection(id: id) { success,data in
+			guard success,let unwrappedIds = data else { return }
+				self.dataManager.getProductDataForIds(ids: unwrappedIds){ success, data in
+					guard success, let unwrappedData = data, self.setTableData(forData: unwrappedData) else { return }
 					self.productsTableView.reloadData()
-					self.productsTableView.rowHeight = 100
-
-					for product in self.collectionsData! {
-						var total = 0
-								for variants in product.variants! {
-									let inventory = variants["inventory_quantity"] as! Int
-									total += inventory
-
-								}
-
-						
-						self.tableViewData.append(cellData(opened: false, title: product.title!,total:"Total Inventory: \(total)",description: product.bodyHtml!,imageUrl:product.image!["src"] as! String, sectionData: product.variants!))
-
-
-					}
-					self.productsTableView.reloadData()
-
 				}
-			}
 
 		}
 	}
 
 	func numberOfSections(in tableView: UITableView) -> Int {
-		dump(tableViewData.count)
 		return tableViewData.count
 	}
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		if tableViewData[section].opened == true {
 			return tableViewData[section].sectionData.count + 1
-
 		} else {
 			return 1
 		}
@@ -87,8 +57,7 @@ class CollectionProductsViewController:UIViewController,UITableViewDelegate,UITa
 
 	 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		if indexPath.row == 0 {
-			let cell = productsTableView.dequeueReusableCell(withIdentifier: "productsTableViewCell", for: indexPath as IndexPath) as! ProductsTableViewCell
-
+			guard let cell = productsTableView.dequeueReusableCell(withIdentifier: "productsTableViewCell", for: indexPath as IndexPath) as? ProductsTableViewCell else { return UITableViewCell() }
 			cell.title.text = tableViewData[indexPath.section].title
 					cell.totalLabel.text = tableViewData[indexPath.section].description
 					cell.collectionTitleLabel.text = tableViewData[indexPath.section].total
@@ -97,19 +66,13 @@ class CollectionProductsViewController:UIViewController,UITableViewDelegate,UITa
 								cell.productImage.image = image
 							}
 						}
-			cell.separatorInset = .zero
 			return cell
 		} else {
-			let cell = productsTableView.dequeueReusableCell(withIdentifier: "variantsCell", for: indexPath as IndexPath) as! VariantsCell
+			guard let cell = productsTableView.dequeueReusableCell(withIdentifier: "variantsCell", for: indexPath as IndexPath) as? VariantsCell else { return UITableViewCell() }
 			cell.title.text = tableViewData[indexPath.section].sectionData[indexPath.row - 1]["title"] as? String
-			cell.price.text = "$\(tableViewData[indexPath.section].sectionData[indexPath.row - 1]["price"] as! String)"
-			cell.inventory.text = "Inventory:\(String(describing: tableViewData[indexPath.section].sectionData[indexPath.row - 1]["inventory_quantity"] as! Int))"
-			cell.accessoryType = .none
-//			Alamofire.request(tableViewData[indexPath.section].sectionData[indexPath.row - 1]).responseImage { response in
-//				if let image = response.result.value {
-//					cell.productImage.image = image
-//				}
-//			}
+			guard let price = tableViewData[indexPath.section].sectionData[indexPath.row - 1]["price"] as? String,let inventory =  tableViewData[indexPath.section].sectionData[indexPath.row - 1]["inventory_quantity"] as? Int else { return cell}
+			cell.price.text = "$\(price)"
+			cell.inventory.text = "Inventory:\(inventory)"
 			return cell
 		}
 
@@ -136,68 +99,18 @@ class CollectionProductsViewController:UIViewController,UITableViewDelegate,UITa
 		}
 	}
 
-
-
-
-
-
-
-//
-//	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//		let cell = productsTableView.dequeueReusableCell(withIdentifier: "productsTableViewCell", for: indexPath as IndexPath) as! ProductsTableViewCell
-//		let collection = collectionsData![indexPath.row]
-//		cell.title.text = collection.title
-//		cell.totalLabel.text = "Total Inventory: \(self.getTotalInventory(ForProduct: indexPath.row))"
-//		cell.collectionTitleLabel.text = collection.title
-//
-//
-//		guard let imageUrl = collection.image?["src"] as? String else {
-//			return cell
-//		}
-//
-//		Alamofire.request(imageUrl).responseImage { response in
-//			if let image = response.result.value {
-//				cell.productImage.image = image
-//			}
-//		}
-//
-//		return cell
-//	}
-//
-//	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//		guard let count = self.collectionsData?.count else {
-//			return 0
-//		}
-//		return count
-//	}
-//
-//
-//	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//		//		let collection = collectionsData![indexPath.row]
-//		//		self.performSegueToVote(id:collection.id!)
-//		navigationController?.popViewController(animated: false)
-//	}
-//
-//	func performSegueToVote(id:Int) {
-//		let myVC = storyboard?.instantiateViewController(withIdentifier: "collectionProductsViewController") as! CollectionProductsViewController
-//
-//		myVC.collectionID = id
-//		navigationController?.pushViewController(myVC, animated: true)
-//	}
-//
-//	private func getTotalInventory(ForProduct position:Int)->Int {
-//		let product = self.collectionsData![position]
-//		var total = 0
-//		for variants in product.variants! {
-//			let inventory = variants["inventory_quantity"] as! Int
-//			total += inventory
-//
-//		}
-//		return total
-//
-//	}
-
-
-
+	private func setTableData(forData data:[Product] )-> Bool {
+		for product in data {
+			var total = 0
+			guard let variants = product.variants else { return false }
+			for variant in variants {
+				guard let quantity = variant["inventory_quantity"] as? Int else {return false}
+				total += quantity
+			}
+			guard let title = product.title, let description = product.bodyHtml, let image = product.image,let imageUrl = image["src"] as? String else { return false }
+			self.tableViewData.append(cellData(opened: false, title: title,total:"Total Inventory: \(total)",description: description,imageUrl: imageUrl, sectionData: variants))
+		}
+		return true
+	}
 
 }
