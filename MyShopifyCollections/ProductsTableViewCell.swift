4dd6ed57fe9c6e354fba74dd8e03b825b8f88510
Pawan Patel <patelpawan1996@hotmail.com>
2019-01-11 07:40:36 -0500

    gitignore file

---
+++ MyShopifyCollections/ProductsTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ProductsTableViewCell.swift
+//  MyShopifyCollections
+//
+//  Created by Pawan Patel on 2019-01-10.
+//  Copyright © 2019 PIM. All rights reserved.
+//
+
+import Foundation
---
+++ MyShopifyCollections/VariantsCell.swift
@@ -0,0 +1,9 @@
+//
+//  VariantsCell.swift
+//  MyShopifyCollections
+//
+//  Created by Pawan Patel on 2019-01-11.
+//  Copyright © 2019 PIM. All rights reserved.
+//
+
+import Foundation
